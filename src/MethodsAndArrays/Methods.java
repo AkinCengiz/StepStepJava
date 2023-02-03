ea3c2aade7ad8a074c3fced5f1d191f4cdf46232
Akın CENGİZ <akincengiz@outlook.com.tr>
2023-02-03 13:33:01 +0300

    MethodsAndArrays package eklendi.

---
+++ src/MethodsAndArrays/Methods.java
@@ -0,0 +1,25 @@
+package MethodsAndArrays;
+
+public class Methods {
+
+    public static void main(String[] args) {
+
+    }
+    public static int Added(int a, int b)
+    {
+        return a + b;
+    }
+    public static int Added(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+    public static double Added(double a, double b)
+    {
+        return a + b;
+    }
+    public static double Added(double a, double b, double c)
+    {
+        return a + b +c;
+    }
+
+}
